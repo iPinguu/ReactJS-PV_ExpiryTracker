d4c226ecbf67a4adcf95464175bf328b2792c23a
Franz Luiz <luizdsy@gmail.com>
2024-07-20 21:54:21 -0400

    initial commit

---
+++ src/app/components/logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DisplayImage= () => {
+    return (
+        <div>
+            <img src="/images/PV_logo_transparent.png" alt="PetValu Logo"/>
+        </div>
+    );
+};
+
+export default DisplayImage;
